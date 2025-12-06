900040ee405638dc498af7084f64db8c16b30757
Harry Jung <harry@Harrys-MacBook-Pro.local>
2025-12-07 03:40:48 +0900

    Fix layout sizing for 80% zoom

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -4,7 +4,7 @@ import { Header } from "../components/Header";
 
 export function DashboardLayout() {
     return (
-        <div className="flex h-screen bg-background text-foreground overflow-hidden">
+        <div className="flex h-[125vh] w-[125vw] bg-background text-foreground overflow-hidden">
             <Sidebar />
             <div className="flex-1 flex flex-col overflow-hidden">
                 <Header />
